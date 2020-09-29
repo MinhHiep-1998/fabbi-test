cd0d9db86957b2a976c1c70db31bc7cb3aed469b
hackintosh-linxhq <hiepdtm@linxhq.com>
2020-09-29 11:51:54 +0700

    Updates

---
+++ precache-manifest.4d9009f963a122671f8c4e583c1aaa8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c44fd3ff2b114b0e3506406ca01fbc",
+    "url": "/fabbi-test/index.html"
+  },
+  {
+    "revision": "d36a2a7d0b69e4980c87",
+    "url": "/fabbi-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11c4d7d69f93ff31e3b6",
+    "url": "/fabbi-test/static/js/2.f67fd4dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fabbi-test/static/js/2.f67fd4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36a2a7d0b69e4980c87",
+    "url": "/fabbi-test/static/js/main.9cea0d44.chunk.js"
+  },
+  {
+    "revision": "1810f0a1c330fce594ed",
+    "url": "/fabbi-test/static/js/runtime-main.bd7f37b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fabbi-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
